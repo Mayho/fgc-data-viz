9463a154b9791dfa5e38297bdbe33a0a2ac0e4de
Mehul Patel <mehul8@gmail.com>
2016-07-02 11:51:44 -0700

    feat(Home): add home comp and maincontainer

---
+++ app/components/Home.js
@@ -0,0 +1,13 @@
+var React = require('react');
+var ReactRouter = require('react-router');
+var MainContainer = require('./MainContainer');
+
+function Home () {
+  return (
+    <MainContainer>
+      <h1>ayee its lit! 🔥</h1>
+    </MainContainer>
+  )
+}
+
+module.exports = Home;
